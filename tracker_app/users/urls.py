dce8cfd90b68bdb768cf59bfd43f93ec50a28fb8
Sloan <undone1@me.com>
2020-09-01 20:06:30 -0400

    register&login

--- tracker_app/users/urls.py
+++ tracker_app/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from . import views
 
+app_name = "users"
+
 urlpatterns = [
+    path(" ", include("django.contrib.auth.urls")),
     path("create/", views.signup, name="create_user"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
--- tracker_app/users/views.py
+++ tracker_app/users/views.py
@@ -9,6 +9,28 @@ from . import forms
 import django.contrib.auth as dj_auth
 import django.contrib.auth.views as auth_views
 from .models import User
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    """Register a new user"""
+    if request.method != 'POST':
+        #Display blank registration form .
+        form = UserCreationForm()
+    else:
+        #Process completed form.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            #Log the user in and then redirect to home page.
+            login(request, new_user)
+            return redirect('tracker_app:index')
+
+    #Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, registration/register.html', context)        
+
 
 
 def signup(request):
