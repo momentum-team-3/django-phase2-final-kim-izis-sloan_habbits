425dca6d1517c49fe93b8a0971c55dca6c1e3ad1
Sloan <undone1@me.com>
2020-09-01 20:07:39 -0400

    register&login

--- tracker_app/users/urls.py
+++ tracker_app/users/urls.py
@@ -5,6 +5,7 @@ app_name = "users"
 
 urlpatterns = [
     path(" ", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
     path("create/", views.signup, name="create_user"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
